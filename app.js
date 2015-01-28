3874b0d38faeb27fa3062dea762bfa0ab7960165
Loquen Jones <lrjones416@gmail.com>
2015-01-28 11:54:35 -0800

    added new flyer and gallery html

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 $(document).ready(function(){
-	var userId;
-	//initialize soundcloud API with client ID
-	SC.initialize({
-		client_id: "2fb00e038d2c0bf801c77c8ed77588c5",
-   		//redirect_uri: "http://example.com/callback.html",
-	});	
-
-    var userUrl = 'https://soundcloud.com/alphexandb-ripmusic';
-	$.get('http://api.soundcloud.com/resolve.json?url=' + userUrl + '&client_id=2fb00e038d2c0bf801c77c8ed77588c5', 
-  		function (result) {
-    		//after we resolve the username, we grab all the tacks of the user and display the first 3
-    		userId = result.id;
-    		console.log(userId);
-    		SC.get("/users/" + userId + "/tracks" , {limit:3}, function(tracks){
-    			var track = tracks[0];
-    			SC.oEmbed(track.uri,{maxheight: 100}, document.getElementById("displayMusic"));
-			});
-  		}
-  	);	
+		
 
   $("#about").click(function(){
     //first hide all other divs
@@ -25,13 +7,13 @@ $(document).ready(function(){
     $(".artistbio").hide();
     $(".events").hide();
     $(".hot").hide();
-    // $(".gallery").hide();
+    $(".gallery").hide();
     $(".aboutblurb").show();
     $("#about").addClass("hover");
     $("#artist").removeClass("hover");
     $("#event").removeClass("hover");
     $("#hot").removeClass("hover");
-    // $("#gallery").removeClass("hover");
+    $("#gallery").removeClass("hover");
   });
 
   $("#artist").click(function(){
@@ -40,13 +22,13 @@ $(document).ready(function(){
     $(".aboutblurb").hide();
     $(".events").hide();
     $(".hot").hide();
-    // $(".gallery").hide();
+    $(".gallery").hide();
     $(".artistbio").show();
     $("#artist").addClass("hover");
     $("#about").removeClass("hover");
     $("#event").removeClass("hover");
     $("#hot").removeClass("hover");
-    // $("#gallery").removeClass("hover");
+    $("#gallery").removeClass("hover");
   });
 
   $("#event").click(function(){
@@ -55,13 +37,13 @@ $(document).ready(function(){
     $(".aboutblurb").hide();
     $(".artistbio").hide();
     $(".hot").hide();
-    // $(".gallery").hide();
+    $(".gallery").hide();
     $(".events").show();
     $("#event").addClass("hover");
     $("#about").removeClass("hover");
     $("#artist").removeClass("hover");
     $("#hot").removeClass("hover");
-    // $("#gallery").removeClass("hover");
+    $("#gallery").removeClass("hover");
   });
 
   $("#hot").click(function(){
@@ -70,31 +52,60 @@ $(document).ready(function(){
     $(".aboutblurb").hide();
     $(".events").hide();
     $(".artistbio").hide();
-    // $(".gallery").hide();
+    $(".gallery").hide();
     $(".hot").show();
     $("#hot").addClass("hover");
     $("#about").removeClass("hover");
     $("#event").removeClass("hover");
     $("#artist").removeClass("hover");
-    // $("#gallery").removeClass("hover");
+    $("#gallery").removeClass("hover");
   });
 
-  // $("#gallery").click(function(){
-  //   //first hide all other divs
-  //   //then load galleria and run it 
-  //   $(".aboutblurb").hide();
-  //   $(".events").hide();
-  //   $(".artistbio").hide();
-  //   $(".hot").hide();
-  //   $(".gallery").show();
-  //   $("#gallery").addClass("hover");
-  //   $("#about").removeClass("hover");
-  //   $("#event").removeClass("hover");
-  //   $("#artist").removeClass("hover");
-  //   $("#hot").removeClass("hover");
+  
+
+  var galleryImages = [
+    {
+        image: 'images/gallery/4onthefloor.jpg',
+        title: 'Four on the Floor Thursday',
+    },
+    {
+        image: 'images/gallery/alphexbrip.jpg',
+        title: 'Alphex and B-Rip',
+    },
+    {
+        image: 'images/gallery/burningmandecomp.jpg',
+        title: 'Burning Man Decompression',
+    },
+    {
+        image: 'images/gallery/cam.jpg',
+        title: 'Cameron',
+    },
+    {
+        image: 'images/gallery/camilo.jpg',
+        title: 'Camilo',
+    }
+  ];
+
+  $("#gallery").click(function(){
+      //first hide all other divs
+      //then load galleria and run it 
+      $(".aboutblurb").hide();
+      $(".events").hide();
+      $(".artistbio").hide();
+      $(".hot").hide();
+      $(".gallery").show();
+      $("#gallery").addClass("hover");
+      $("#about").removeClass("hover");
+      $("#event").removeClass("hover");
+      $("#artist").removeClass("hover");
+      $("#hot").removeClass("hover");
     
-  //   //Load the galleria class theme 
-  //   Galleria.loadTheme('galleria/themse/classic/galleria.classic.min.js');
-  //   Galleria.run('.galleria');
-  // });
+      // //Load the galleria class theme and run gallery
+      // Galleria.loadTheme('galleria/themse/classic/galleria.classic.min.js');
+      // Galleria.run('.galleria', {
+      //   dataSource:galleryImages
+      // )};
+    
+    });
+
 });
